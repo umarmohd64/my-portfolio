9816985809ca9c26d7cafbf1d9b0e285d2067558
Mohd Umar <u9889718044@gmail.com>
2024-01-25 21:38:02 +0530

    changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,12 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  purge: {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/*.{js,ts,jsx,tsx,mdx}',
+    ],
+  },
   theme: {
     extend: {
       fontFamily:{
